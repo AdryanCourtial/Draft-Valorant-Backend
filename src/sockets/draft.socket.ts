706f5688de028a4d00e963dddf6c438cb9d54979
AdryanCourtial <adryan.courtial@ynov.com>
2025-06-30 21:17:56 +0200

    :contruction: Bonne avancé sur le système de draft

--- src/sockets/draft.socket.ts
+++ src/sockets/draft.socket.ts
@@ -1,7 +1,7 @@
 import { Server, Socket } from "socket.io";
-import { generateShortId, generateUuid } from "../utils/utils";
-import type { Room, Side, SideTeam } from 'drafter-valorant-types';
-import { StateRoomGame } from 'drafter-valorant-types';
+import { ChangeSidePickOrBan, FindFirstNullInArray, generateShortId, generateUuid, VerifyIfChampIsOpen } from "../utils/utils";
+import type { Agent, Room, Side, SideTeam } from 'drafter-valorant-types';
+import { referenceOrderDraftAction, StateRoomGame } from 'drafter-valorant-types';
 
 let rooms: { [roomId: string]: Room } = {}; 
 
@@ -13,12 +13,17 @@ export const draftSocketHandler = (io: Server, socket: Socket) => {
     const uuid = generateUuid();
     const public_link = process.env.FRONT_URL+  `/draft` + `/${uuid}`;
 
+    const createNewDraftSession = () => {
+      return JSON.parse(JSON.stringify(referenceOrderDraftAction));
+    };
+
     try {
       const room: Room = {
         id: generateShortId(),
         uuid,
         public_link,
         map_selected: mapId,
+        draft_session: createNewDraftSession(),
         state: StateRoomGame.WAITING,
         creator_id: creatorId,
         spectators: [],
@@ -26,24 +31,27 @@ export const draftSocketHandler = (io: Server, socket: Socket) => {
           name: attackers,
           team_leader: 0,
           isReady: false,
-          agents: [],
-          bans: []
+          agents: Array(5).fill(null),
+          bans: Array(2).fill(null),
         },
         defenders_side: {
           name: defenders,
           team_leader: 0,
           isReady: false,
-          agents: [],
-          bans: []
+          agents: Array(5).fill(null),
+          bans: Array(2).fill(null)
         }
       };
     
       rooms[uuid] = room;
 
+      console.log(room)
       socket.join(room.uuid);
       io.to(room.uuid).emit("room-created", room);
 
-      console.log('je suis rooms', rooms);
+      // console.log('je suis rooms', rooms);
+      // console.log('je suis rooms la draft de réference', rooms);
+
       
     } catch (error) {
       console.error("Error creating room:", error);
@@ -52,14 +60,14 @@ export const draftSocketHandler = (io: Server, socket: Socket) => {
   });
 
   socket.on("getRoom", (roomId: string) => {
-    const room = rooms[roomId];
-    if (!room) {
-      socket.emit("error", { message: "Room introuvable" });
-      return;
-    }
-    socket.join(roomId);
-    console.log(`🔵 ${socket.id} a rejoint la room ${roomId}`)
-    socket.emit("room-updated", room)
+      const room = rooms[roomId];
+      if (!room) {
+        socket.emit("error", { message: "Room introuvable" });
+        return;
+      }
+      socket.join(roomId);
+      console.log(`🔵 ${socket.id} a rejoint la room ${roomId}`)
+      socket.emit("room-updated", room)
     });
   
     
@@ -83,12 +91,71 @@ export const draftSocketHandler = (io: Server, socket: Socket) => {
       io.to(roomId).emit("room-updated", room);
     });
 
+    socket.on("test-is-ready", ({ roomId }: { roomId: string}) => {
+      const room = rooms[roomId];
 
-  socket.on("pick-agent", ({ roomId, agent }) => {
-    io.to(roomId).emit("agent-picked", { agent, by: socket.id });
+      console.log("TOUR ACTUELLE", room.draft_session.curent_turn)
+
+
+      room.state = StateRoomGame.RUNNING;
+      room.draft_session.curent_turn += 1
+
+      console.log("TOUR PROCHAIN", room.draft_session.curent_turn)
+
+
+      io.to(roomId).emit("start-draft", room );
+    });
+
+    socket.on("confirm-round", ({ roomId, agent }: { roomId: string, agent: Agent}) => {
+      const room = rooms[roomId];
+
+      const listChampSelected = room.attackers_side.agents.concat(room.attackers_side.bans, room.defenders_side.agents, room.defenders_side.bans)
+      console.log("🔴JE SUIS LARRAY DES PERSONNAGE DEJA UTILIsé", listChampSelected)
+
+      if (VerifyIfChampIsOpen(room, agent)) {
+        console.log("Ce Champion à déjà été pris")
+        socket.emit("error", { message: "Ce champion è déjà été pick ou ban" });
+        return
+      }
+
+      const curent_turn = room.draft_session.draft_actions.find((value) => {
+        return value.turn === room.draft_session.curent_turn
+      })
+
+      if (!curent_turn) return
+
+      const side_to_change = curent_turn.team
+
+      if (curent_turn.type === "ban") {
+
+        ChangeSidePickOrBan(room, side_to_change, agent, "ban")
+
+      } else {
+
+        ChangeSidePickOrBan(room, side_to_change, agent, "pick")
+
+      }
+
+      room.draft_session.curent_turn += 1
+
+      console.log("POCHAIN TOUR", room.draft_session.curent_turn)
+
+      io.to(roomId).emit("agent-picked", room );
+    });
+
+
+  // socket.on("pick-agent", ({ roomId, agent }) => {
+  //   io.to(roomId).emit("agent-picked", { agent, by: socket.id });
+  // });
+
+  socket.on("leaveAllRooms", () => {
+    const roomsToLeave = Array.from(socket.rooms).filter((room) => room !== socket.id);
+    roomsToLeave.forEach((room) => socket.leave(room));
+    console.log(`🧹 ${socket.id} a quitté toutes ses rooms personnalisées`);
   });
-
+  
   socket.on("disconnect", () => {
     console.log(`🔴 [socket] ${socket.id} déconnecté`);
+    
   });
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { customAlphabet } from 'nanoid';
+import { Agent, DraftAction, Room, SideTeam, StateDraft } from 'drafter-valorant-types';
 
 // Generates a UUID using the uuid library
 export const generateUuid = () => uuidv4();
@@ -8,3 +9,29 @@ export const generateUuid = () => uuidv4();
 const nanoid = customAlphabet('1234567890abcdef', 6);
 
 export const generateShortId = () => nanoid();
+
+export const FindFirstNullInArray = (array: Array<any>) => {
+    return array.findIndex((value) => value === null)
+}
+
+export const VerifyIfChampIsOpen = (room: Room, agent: Agent) => {
+    const listChampSelected = [
+    ...room.attackers_side.agents,
+    ...room.attackers_side.bans,
+    ...room.defenders_side.agents,
+    ...room.defenders_side.bans,
+    ];
+
+    return listChampSelected.filter(a => a !== null)
+    .some(a => a!.id === agent.id);
+
+}
+
+export const ChangeSidePickOrBan = (room: Room, side_to_change: SideTeam, agent: Agent, type: StateDraft) => {
+
+    const array = type === "ban" ? "bans" : "agents"
+
+    const index_agent = FindFirstNullInArray(room[side_to_change][array])
+    room[side_to_change][array][index_agent] = agent
+
+}
